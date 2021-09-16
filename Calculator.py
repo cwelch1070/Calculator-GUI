83f9a74ba01091186f530761f4ff581e1485c8b1
cwelch1070 <90748206+cwelch1070@users.noreply.github.com>
2021-09-15 23:19:16 -0500

    Update Calculator.py
    Added multiplication, subtraction, and division functionality.

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,7 @@
 #https://www.youtube.com/watch?v=YXPyB4XeYLA
 #stoped at 1:06:47
 from tkinter import*
+from typing import Match
 
 root = Tk()
 #Changes name at the top when window opens
@@ -24,14 +25,51 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
+    global math 
+    math = "addition"
     f_num = int(first_number)
     e.delete(0, END)
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global math 
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def button_divide():
+    first_number = e.get()
+    global f_num
+    global math 
+    math = "divide"
+    f_num = int(first_number)
+    e.delete(0, END)
+    
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    global math 
+    math = "subtract"
+    f_num = int(first_number)
+    e.delete(0, END)
+    
 
 #Funtion to tell the equal button what to do
 def button_equal():
     second_number = e.get()
     e.delete(0, END)
-    e.insert(0, f_num + int(second_number))
+    if(math == "addition"):
+         e.insert(0, f_num + int(second_number))
+    
+    if(math == "multiplication"):
+         e.insert(0, f_num * int(second_number))
+         
+    if(math == "divide"):
+         e.insert(0, f_num / int(second_number))
+         
+    if(math == "subtract"):
+         e.insert(0, f_num - int(second_number))
 
 #customizes each button and pass the specific data through that each is intended for
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
@@ -48,6 +86,9 @@ button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click
 button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
 button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+button_multiply = Button(root, text="x", padx=39, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=39, pady=20, command=button_divide)
+button_subtract = Button(root, text="-", padx=39, pady=20, command=button_subtract)
 
 #Decides the postion where each button is located on the screen to make look like a calculator
 button_1.grid(row=3, column=0)
@@ -64,9 +105,12 @@ button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
 
-button_clear.grid(row=4, column=1, columnspan=2)
-button_add.grid(row=5, column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
+button_clear.grid(row=5, column=0, columnspan=2)
+button_add.grid(row=4, column=1)
+button_subtract.grid(row=6, column=2)
+button_multiply.grid(row=4, column=2)
+button_divide.grid(row=5, column=2)
+button_equal.grid(row=6, column=0, columnspan=2)
 
 #main loop to keep window up on screen
 root.mainloop()
